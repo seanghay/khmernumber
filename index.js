5169deb37b4a31b72f12e4bff47e7a230a4a8e90
Seanghay Yath <seanghay.dev@gmail.com>
2022-10-17 00:32:39 +0700

    minified

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-exports.toASCII = s => [...s].reduce((p, c) => p + (c = c.charCodeAt(0), String.fromCharCode(c - 6064 * ((c - 6112) * (c - 6121) < 1 ^ 0))), '');
-exports.toKhmer = s => [...s].reduce((p, c) => p + (c = c.charCodeAt(0), String.fromCharCode(c + 6064 * ((c - 48) * (c - 57) < 1 ^ 0))), '');
+const f = (s, l, r, op) => [...s].reduce((p, c) => p + (c = c.charCodeAt(0), String.fromCharCode(c - op * 6064 * ((c - l) * (c - r) < 1 ^ 0))), '');
+exports.toASCII = s => f(s, 6112, 6121, 1)
+exports.toKhmer = s => f(s, 48, 57, -1)
--- index.mjs
+++ index.mjs
@@ -1,2 +1,3 @@
-export const toASCII = s => [...s].reduce((p, c) => p + (c = c.charCodeAt(0), String.fromCharCode(c - 6064 * ((c - 6112) * (c - 6121) < 1 ^ 0))), '');
-export const toKhmer = s => [...s].reduce((p, c) => p + (c = c.charCodeAt(0), String.fromCharCode(c + 6064 * ((c - 48) * (c - 57) < 1 ^ 0))), ''); 
+const f = (s, l, r, op) => [...s].reduce((p, c) => p + (c = c.charCodeAt(0), String.fromCharCode(c - op * 6064 * ((c - l) * (c - r) < 1 ^ 0))), '');
+export const toASCII = s => f(s, 6112, 6121, 1);
+export const toKhmer = s => f(s, 40, 57, -1);
