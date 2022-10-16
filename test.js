ef704b8e8b285effb02d562876ba9ef660d1f348
Seanghay Yath <seanghay.dev@gmail.com>
2022-10-17 00:33:38 +0700

    add letter

--- test.js
+++ test.js
@@ -2,9 +2,9 @@
   const { equal } = await import('node:assert')
   const { toASCII, toKhmer } = await import('./index.mjs');
   const { toASCII: toASCII2, toKhmer: toKhmer2 } = await import('./index.js');
-  equal('01234567890123456789', toASCII('០១២៣៤៥៦៧៨៩0123456789'))
-  equal('01234567890123456789', toASCII2('០១២៣៤៥៦៧៨៩0123456789'))
-  equal('០១២៣៤៥៦៧៨៩០១២៣៤៥៦៧៨៩', toKhmer('0123456789០១២៣៤៥៦៧៨៩'))
-  equal('០១២៣៤៥៦៧៨៩០១២៣៤៥៦៧៨៩', toKhmer2('0123456789០១២៣៤៥៦៧៨៩'))
+  equal('01234567890123456789abc', toASCII('០១២៣៤៥៦៧៨៩0123456789abc'))
+  equal('01234567890123456789abc', toASCII2('០១២៣៤៥៦៧៨៩0123456789abc'))
+  equal('០១២៣៤៥៦៧៨៩០១២៣៤៥៦៧៨៩abc', toKhmer('0123456789០១២៣៤៥៦៧៨៩abc'))
+  equal('០១២៣៤៥៦៧៨៩០១២៣៤៥៦៧៨៩abc', toKhmer2('0123456789០១២៣៤៥៦៧៨៩abc'))
 })()
 
